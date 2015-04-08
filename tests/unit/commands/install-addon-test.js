9465b056d852b8e2bc9b95c9d4bf7a1d430962f4
Daniel Ochoa <daniel@poeticsystems.com>
2015-04-08 18:02:10 -0500

    `install:addon` will show a deprecation warning then it will run the `install` command. Changed the verbage on install-npm.

--- tests/unit/commands/install-addon-test.js
+++ tests/unit/commands/install-addon-test.js
@@ -3,10 +3,31 @@
 var expect = require('chai').expect;
 var InstallAddonCommand = require('../../../lib/commands/install-addon');
 var commandOptions = require('../../factories/command-options');
+var AddonInstall = require('../../../lib/tasks/addon-install');
+var Task = require('../../../lib/models/task');
+var Promise = require('../../../lib/ext/promise');
+var stub = require('../../helpers/stub').stub;
+var Project = require('../../../lib/models/project');
 
 describe('install:addon command', function() {
-  var command, options, msg;
-  before(function() {
+  var command, options, tasks, npmInstance, generateBlueprintInstance;
+
+  beforeEach(function() {
+    tasks = {
+      AddonInstall: AddonInstall,
+      NpmInstall: Task.extend({
+        init: function() {
+          npmInstance = this;
+        }
+      }),
+
+      GenerateFromBlueprint: Task.extend({
+        init: function() {
+          generateBlueprintInstance = this;
+        }
+      })
+    };
+
     options = commandOptions({
       settings: {},
       project: {
@@ -15,33 +36,53 @@ describe('install:addon command', function() {
         },
         isEmberCLIProject: function() {
           return true;
-        }
-      }
+        },
+        initializeAddons: function() {  },
+        reloadAddons: function() {
+          this.addons = [{
+            pkg: {
+              name: 'ember-cli-photoswipe',
+              'ember-addon': {
+                defaultBlueprint: 'photoswipe'
+              }
+            }
+          }];
+        },
+
+        findAddonByName: Project.prototype.findAddonByName
+      },
+
+      tasks: tasks
     });
+
+    stub(tasks.NpmInstall.prototype, 'run', Promise.resolve());
+    stub(tasks.GenerateFromBlueprint.prototype, 'run', Promise.resolve());
 
     command = new InstallAddonCommand(options);
 
-    msg  = 'This command has been deprecated. Please use `ember install ';
-    msg += '<addonName>` instead.';
   });
 
-  describe('with args', function() {
-    it('gives a helpful message if no arguments are passed', function() {
-      return command.validateAndRun(['ember-cli-cordova']).then(function() {
-        expect(false, 'should reject with error');
-      }).catch(function(err) {
-        expect(err.message).to.equal(msg, 'expect error to have a helpful message');
-      });
-    });
+  afterEach(function() {
+    tasks.NpmInstall.prototype.run.restore();
+    tasks.GenerateFromBlueprint.prototype.run.restore();
   });
 
-  describe('without args', function() {
-    it('gives a helpful message if no arguments are passed', function() {
-      return command.validateAndRun([]).then(function() {
-        expect(false, 'should reject with error');
-      }).catch(function(err) {
-        expect(err.message).to.equal(msg, 'expect error to have a helpful message');
-      });
+
+  it('will show a deprecation warning', function() {
+    return command.validateAndRun(['ember-cli-photoswipe']).then(function() {
+      var msg  = 'This command has been deprecated. Please use `ember install ';
+      msg     += '<addonName>` instead.';
+
+      expect(command.ui.output).to.include(msg);
+
+      expect(npmInstance.ui, 'ui was set');
+      expect(npmInstance.project, 'project was set');
+      expect(npmInstance.analytics, 'analytics was set');
+
+      expect(generateBlueprintInstance.ui, 'ui was set');
+      expect(generateBlueprintInstance.project, 'project was set');
+      expect(generateBlueprintInstance.analytics, 'analytics was set');
+
     });
   });
 });
